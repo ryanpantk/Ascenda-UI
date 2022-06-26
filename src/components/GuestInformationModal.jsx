6af4bef182d4ac8806f89d74b961da8ff5432c28
Ryan <ryanpantk@gmail.com>
2022-06-27 00:22:19 +0800

    Add Mock Booking Data

--- src/components/GuestInformationModal.jsx
+++ src/components/GuestInformationModal.jsx
@@ -1,8 +1,21 @@
 import {Button, Card, Form, Input, Select, Col, Row} from 'antd';
 import ReCAPTCHA from 'react-google-recaptcha';
-import JSONDATA from '../countryCode.json'
+import JSONDATA from '../countryCode.json';
+import { stripeCheckout } from '../api';
+
+// import { useSelector, useDispatch } from 'react-redux';
 const {TextArea} = Input;
 const { Option } = Select;
+const fakeData = {
+    hotelName: "Fullerton Hotel Singapore",
+    roomType: "Deluxe Plus",
+    numberOfRoom: 2,
+    numberOfAdult: 2,
+    numberOfChildren: 2,
+    checkinDate: "10/12/2022",
+    checkoutDate: "11/12/2022",
+    totalCost: 12.40
+}
 
 const GuestInformationModal = () => {
     const [form] = Form.useForm();
@@ -60,8 +73,38 @@ const GuestInformationModal = () => {
     return (
         <Row>
             <Col span={10} offset={7} style={{marginTop: 16, marginBottom: 16}}>
-                <Card title= {<h3> <b> Booking Summary</b></h3>} style={{fontWeight: "bold"}} >
+                <Card title= {<h3> <b> Booking Summary</b></h3>} >
+                    <div style={{textAlign: "left"}}>
+                        <Row>
+                            <span style={{display:"block", fontWeight: "bold",fontSize: "large"}}>{fakeData.hotelName}</span>
+                        </Row>
+                        <Row>
+                            <span style={{display:"block", fontWeight: "bold",fontSize: "medium"}}>{fakeData.roomType}</span>
+                        </Row>
+                        <Row>
+                            <p style={{display:"block", fontWeight: "bold",fontSize: "medium"}}>${fakeData.totalCost.toFixed(2)}</p>
+                        </Row>
+                        <Row>
+                            <Col span={7} offset={0}>
+                                <p> <b>Number of Room :</b> {fakeData.numberOfRoom}</p>
+                            </Col>
+                            <Col span={7} offset={1}>
+                                <p><b>Number of Adult:</b> {fakeData.numberOfAdult}</p>
+                            </Col>
+                            <Col span={7} offset={1}>
+                                <p><b>Number of Children:</b> {fakeData.numberOfChildren}</p>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col span={7} offset={0}>
+                                <p><b>Check-in Date:</b> {fakeData.checkinDate}</p>
+                            </Col>
+                            <Col span={7} offset={1}>
+                                <p><b>Checkout-out Date:</b> {fakeData.checkoutDate}</p>
+                            </Col>
+                        </Row>
 
+                    </div>
                 </Card>
             </Col>
             <Col span={10} offset={7} style={{marginTop: 16, marginBottom: 16}}>
@@ -120,7 +163,7 @@ const GuestInformationModal = () => {
                         <Row>
                             <Col span={24} offset={0}>
                                 <Form.Item name="specialRequest" label="Special Request">
-                                    <TextArea rows={4} />
+                                    <TextArea placeholder="Special requests cannot be guaranteed – but the property will do its best to meet your needs." rows={4} />
                                 </Form.Item>
                             </Col>
                         </Row>
@@ -134,7 +177,7 @@ const GuestInformationModal = () => {
                     />
             </Col>
             <Col span={4} offset={10} style={{marginTop: 16, marginBottom: 16}}>
-                <Button type="primary" shape="default" size="large" style={{borderRadius: 15}} htmlType="submit">Proceed to Payment</Button>
+                <Button onClick={stripeCheckout} type="primary" shape="default" size="large" style={{borderRadius: 15}} htmlType="submit">Proceed for  Payment</Button>
             </Col>
         </Row>
     );
