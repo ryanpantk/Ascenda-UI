0346d3c4a7257510548af8b5ee1870ac293e5c2d
Paprika0827 <60343714+Paprika0827@users.noreply.github.com>
2022-07-01 19:34:42 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,85 @@
 import './App.css';
+import { useState } from 'react';
 import DestinationForm from './components/DestinationForm.jsx';
 import BookingPage from './components/BookingPage/BookingPage.jsx';
+import { Button, message, Steps } from 'antd';
+import HotelDisplay from './components/HotelDisplay';
+const { Step } = Steps;
+const steps = [
+  {
+    title: 'Enter Destination',
+  },
+  {
+    title: 'Choose hotel room',
+  },
+  {
+    title: 'Enter Payment Details',
+  },
+];
 
 function App() {
+  const [current, setCurrent] = useState(0);
+  const [destinationData, setDestinationData] = useState(null);
+  const [hotelData, setHotelData] = useState(null);
+
+  const next = () => {
+    setCurrent(current + 1);
+  };
+
+  const prev = () => {
+    setCurrent(current - 1);
+  };
+
+  const handleDestinationSubmit = (data) => {
+    setDestinationData(data);
+    next();
+  }
+
+  const handleStepperClick = (idx) => {
+    if(idx < current){
+      setCurrent(idx);
+    }
+  }
+
+  const handleHotelSelect = (hotel) =>{
+    setHotelData(hotel);
+    next();
+  }
+
   return (
-    <div className="App">
-      {/* <DestinationForm></DestinationForm> */}
-      <BookingPage></BookingPage>
-    </div>
+    <>
+      <Steps className="steps-bar" current={current}>
+        {steps.map((item, idx) => (
+          <Step key={item.title} title={item.title} onClick={(e) => handleStepperClick(idx)}/>
+        ))}
+      </Steps>
+      <div className="steps-content">
+        {current === 0 && (
+          <DestinationForm onSubmit={(e) => handleDestinationSubmit(e)}></DestinationForm>
+        )}
+        {current === 1 && (
+          <HotelDisplay GetHotel={handleHotelSelect} DestinationData={destinationData}/>
+        )}
+        {current === 2 && (
+          <BookingPage></BookingPage>
+        )}
+      </div>
+      <div className="steps-action">
+        {current < steps.length - 1 && (
+          <Button type="primary" onClick={() => next()}>
+            Next
+          </Button>
+        )}
+        {current > 0 && (
+          <Button
+            style={{
+              margin: '0 8px',
+            }}
+            onClick={() => prev()}
+          >Previous</Button>
+        )}
+      </div>
+    </>
   );
 }
 
