3d739edac550631093045aea90310f281aa6488a
Paprika0827 <60343714+Paprika0827@users.noreply.github.com>
2022-07-05 20:58:18 +0800

    Add files via upload

--- src/components/GetHotels.js
+++ src/components/GetHotels.js
@@ -6,7 +6,8 @@ export async function GetHotelList(props){
     
 
     const priceData = await fetch(`https://hotelapi.loyalty.dev/api/hotels/prices?destination_id=${destinationID}&checkin=${checkinDate}&checkout=${checkoutDate}&guests=${guestNumber}&partner_id=${partnerID}`)
-    .then(response => response.json());
+    .then(response => response.json())
+    .catch(err=>console.log(err));
     
     const HotelPriceList =priceData.hotels;
     var HotelList = [];
@@ -21,7 +22,7 @@ export async function GetHotelList(props){
         HotelList.push(data);
             }
         
-    
+    console.log(HotelList);
     return HotelList;
 }
 
