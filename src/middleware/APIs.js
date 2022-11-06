306ba8d8dfa1019df32f13ae0ab1fd036bf6d058
Ryan <ryanpantk@gmail.com>
2022-11-06 23:12:39 +0800

    Replace backend serve URL

--- src/middleware/APIs.js
+++ src/middleware/APIs.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 export async function stripeCheckout() {
 
     let StripePayload = { name: "Hotel", unit_amount: store.getState().roomPrice };
-    let resStripe = await axios.post('https://fluffy-granita-fe4f3b.netlify.app/apis/create-checkout-session', StripePayload)
+    let resStripe = await axios.post('https://breakable-ray-panama-hat.cyclic.app/apis/create-checkout-session', StripePayload)
     let NodePayload = {
         salutation: store.getState().salutation,
         firstName: store.getState().firstName,
@@ -30,25 +30,25 @@ export async function stripeCheckout() {
     let data = resStripe.data;
     let {url, payment_intent} = data;
     NodePayload.stripeID = payment_intent;
-    await axios.post('https://fluffy-granita-fe4f3b.netlify.app/apis/postBooking', NodePayload);
+    await axios.post('https://breakable-ray-panama-hat.cyclic.app/apis/postBooking', NodePayload);
     window.location = url;
 }
 
 export async function hotelListDetails(payload) {
-    const hotelListDetails = await fetch(`https://fluffy-granita-fe4f3b.netlify.app/apis/hotelsDetail/${payload}`);
+    const hotelListDetails = await fetch(`https://breakable-ray-panama-hat.cyclic.app/apis/hotelsDetail/${payload}`);
     return hotelListDetails
 }
 
 export async function viewBooking(payload) {
-    const booking = axios.get("https://fluffy-granita-fe4f3b.netlify.app/apis/viewOneBooking/" + payload);
+    const booking = axios.get("https://breakable-ray-panama-hat.cyclic.app/apis/viewOneBooking/" + payload);
     return booking;
 }
 
 export async function deleteInformation(payload) {
-    await axios.patch(`https://fluffy-granita-fe4f3b.netlify.app/apis//updateOneBooking/${payload}`, {});
+    await axios.patch(`https://breakable-ray-panama-hat.cyclic.app/apis//updateOneBooking/${payload}`, {});
 }
 
 export async function checkBookingCredentials(payload) {
-    const checkBooking = axios.post("https://fluffy-granita-fe4f3b.netlify.app/apis/check-booking-credentials", payload);
+    const checkBooking = axios.post("https://breakable-ray-panama-hat.cyclic.app/apis/check-booking-credentials", payload);
     return checkBooking;
 }
